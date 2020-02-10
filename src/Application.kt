da53681390177098a4e04fb116a4555d5f52cf86
vasiliy566 <vasyaisaev31@gmail.com>
2020-02-10 23:26:52 +0300

    add error handlers and constant class with error messages

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,5 @@
 package com.annotator
 import io.ktor.application.*
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -11,28 +10,37 @@ fun main(args: Array<String>) {
     val annotations = VcfHandler.readDatabase("vcfdb/42demo.tsv")
     val server = embeddedServer(Netty, port = 8080) {
         routing {
-            get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
-            }
-            get("/demo") {
-                call.respondText("HELLO WORLD!")
-            }
-            get("/demoArgs") {
-                call.respondText(call.parameters.get("vcf")!!)
-            }
             // example : http://127.0.0.1:8080/getAnnotation?vcf=chr42+9411197+9411200+C
             get("/getAnnotation"){
-                var raw = call.parameters.get("vcf")!!
-                println(raw)
+                if (call.parameters.isEmpty()){
+                    call.respondText(Constants.getEmptyParameters())
+                }else if (!call.parameters.contains("vcf")){
+                    call.respondText(Constants.getVcfNotExist())
+                }
+                else {
 
-                var parsedData = raw.split(" ")
-                println(parsedData)
+                    var raw = call.parameters.get("vcf")!!
 
-                var unAnnotated = VariatnInfo(parsedData[0],
-                    parsedData[1].toInt(),
-                    parsedData[2].toInt(),
-                    parsedData[3])
-                call.respondText(annotations.get(unAnnotated).toString())
+                    var parsedData = raw.split(" ")
+                    if (parsedData.size != 4){
+                        call.respondText(Constants.getWrongFormat())
+                    }
+                    else {
+
+                        try {
+                            var unAnnotated = VariatnInfo(
+                                parsedData[0],
+                                parsedData[1].toInt(),
+                                parsedData[2].toInt(),
+                                parsedData[3]
+                            )
+                            call.respondText(annotations.get(unAnnotated).toString())
+                        } catch(e: java.lang.NumberFormatException){
+                            call.respondText(Constants.getWrongVcfParam())
+                        }
+
+                    }
+                }
             }
         }
     }
