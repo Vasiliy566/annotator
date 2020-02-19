fd4ea48b21e3ea1a5524e9f1523b9f304e1361dc
vasiliy566 <vasyaisaev31@gmail.com>
2020-02-19 04:59:27 +0300

    just user tabix for fast acess

--- src/Application.kt
+++ src/Application.kt
@@ -1,4 +1,5 @@
 package com.annotator
+
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -6,17 +7,12 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    val annotations = VcfHandler.readDatabase("vcfdb/42.tsv")
 
     val server = embeddedServer(Netty, port = 8089) {
         routing {
             // example :
-            // http://127.0.0.1:8089/annotation?vcf=chr42+9411197+9411200+C
-            get("/annotation"){
-              call.respondText(getAnnotation(call.parameters, annotations))
-            }
             // http://127.0.0.1:8089/fastAnnotation?vcf=chr42+9411197+9411200+C
-            get("/fastAnnotation"){
+            get("/fastAnnotation") {
                 call.respondText(getFastAnnotation(call.parameters))
             }
         }
--- src/RequestHandler.kt
+++ src/RequestHandler.kt
@@ -16,38 +16,6 @@ fun execCmd(cmd: String?): String? {
     }
     return `val`
 }
-fun getAnnotation(params: Parameters, annotations: Map<VariantInfo, String> ):String {
-    if (params.isEmpty()){
-        return Constants.EMPTY_PARAMETERS
-    }else if (!params.contains("vcf")){
-        return Constants.VCF_PARAMETER_NOT_EXIST
-    }
-    else {
-        val raw = params["vcf"]!!
-        val parsedData = raw.split(" ")
-        if (parsedData.size != 4){
-            return Constants.WRONG_FIELDS_AMOUNT
-        }
-        else {
-
-            return try {
-                val unAnnotated = VariantInfo(
-                    parsedData[0],
-                    parsedData[1].toInt(),
-                    parsedData[2].toInt(),
-                    parsedData[3]
-                )
-                if (annotations.containsKey(unAnnotated))
-                    annotations[unAnnotated].toString()
-                else ""
-
-            } catch(e: java.lang.NumberFormatException){
-                Constants.WRONG_VCF_PARAMETERS
-            }
-
-        }
-    }
-}
 
 fun getFastAnnotation(params: Parameters):String{
     if (params.isEmpty()){
