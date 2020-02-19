71238abfdbf7665586fa38c12e0f596a7a65692c
Vasily <vasyaisaev31@gmail.com>
2020-02-19 16:26:29 +0300

    Update Application.kt

--- src/Application.kt
+++ src/Application.kt
@@ -7,8 +7,6 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    val annotations = VcfHandler.readDatabase("vcfdb/42.tsv")
-
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             // example :
