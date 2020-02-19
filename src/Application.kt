3a12569d3006c52885940d9a02f0987e767f107a
Vasily <vasyaisaev31@gmail.com>
2020-02-19 16:15:13 +0300

    Change port to default 8080
    Change port to default 8080

--- src/Application.kt
+++ src/Application.kt
@@ -7,7 +7,7 @@ import io.ktor.server.netty.*
 
 fun main() {
     val annotations = VcfHandler.readDatabase("vcfdb/42demo.tsv")
-    val server = embeddedServer(Netty, port = 8089) {
+    val server = embeddedServer(Netty, port = 8080) {
         routing {
             // example : http://127.0.0.1:8080/annotation?vcf=chr42+9411197+9411200+C
             get("/annotation"){
